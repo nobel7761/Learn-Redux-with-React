492b4cb596ce57463105c45e1463b2d4a09a1c4b
nobel7761 <habiburnobel@gmail.com>
2023-02-18 14:20:33 +0600

    simple counter state changing using raw javascript

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// select dom elements
 const counterEl = document.getElementById("counter");
 const incrementEl = document.getElementById("increment");
 const decrementEl = document.getElementById("decrement");
@@ -7,27 +6,25 @@ const counter2El = document.getElementById("counter2");
 const increment2El = document.getElementById("increment2");
 const decrement2El = document.getElementById("decrement2");
 
-// initial state
 let count = 0;
 let count2 = 0;
 
-// event listeners
 incrementEl.addEventListener("click", () => {
-    count++;
-    counterEl.innerText = count;
+  count++;
+  counterEl.innerText = count;
 });
 
 decrementEl.addEventListener("click", () => {
-    count--;
-    counterEl.innerText = count;
+  count--;
+  counterEl.innerText = count;
 });
 
 increment2El.addEventListener("click", () => {
-    count2++;
-    counter2El.innerText = count2;
+  count2++;
+  counter2El.innerText = count2;
 });
 
 decrement2El.addEventListener("click", () => {
-    count2--;
-    counter2El.innerText = count2;
+  count2--;
+  counter2El.innerText = count2;
 });
