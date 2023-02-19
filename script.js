12a5047d96ab865c12054da6447b065279694e5f
nobel7761 <habiburnobel@gmail.com>
2023-02-20 01:49:32 +0600

    redux applied using raw javascript

--- script.js
+++ script.js
@@ -1,52 +1,52 @@
-// select dom elements
 const counterEl = document.getElementById("counter");
 const incrementEl = document.getElementById("increment");
 const decrementEl = document.getElementById("decrement");
 
-// initial state
+//Redux step 1: declaring initialState
 const initialState = {
-    value: 0,
+  value: 0,
 };
 
-// create reducer function
-function counterReducer(state = initialState, action) {
-    if (action.type === "increment") {
-        return {
-            ...state,
-            value: state.value + 1,
-        };
-    } else if (action.type === "decrement") {
-        return {
-            ...state,
-            value: state.value - 1,
-        };
-    } else {
-        return state;
-    }
-}
+//Redux step 2: declaring reducer function
+const counterReducer = (state = initialState, action) => {
+  if (action.type === "increment") {
+    return {
+      ...state,
+      value: state.value + 1,
+    };
+  } else if (action.type === "decrement") {
+    return {
+      ...state,
+      value: state.value - 1,
+    };
+  } else {
+    return { ...state };
+  }
+};
 
-// create store
+//Redux step 3: creating store
 const store = Redux.createStore(counterReducer);
 
-const render = () => {
-    const state = store.getState();
-    counterEl.innerText = state.value.toString();
-};
-
-// update UI initially
-render();
-
-store.subscribe(render);
-
-// button click listeners
+//Redux step 4: action dispatch
 incrementEl.addEventListener("click", () => {
-    store.dispatch({
-        type: "increment",
-    });
+  store.dispatch({
+    type: "increment",
+  });
 });
 
 decrementEl.addEventListener("click", () => {
-    store.dispatch({
-        type: "decrement",
-    });
+  store.dispatch({
+    type: "decrement",
+  });
 });
+
+const render = () => {
+  const latestState = store.getState();
+  counterEl.innerText = latestState.value;
+};
+
+//for showing default data manually
+render();
+
+//Redux step 5: subscribe
+store.subscribe(render);
