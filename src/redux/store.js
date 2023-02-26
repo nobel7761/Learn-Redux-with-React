9775c1fd6a6242ad3e18cc942b3792b4d15a2771
nobel7761 <habiburnobel@gmail.com>
2023-02-27 01:26:01 +0600

    devtools implemented

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,12 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import logger from "redux-logger";
+import { myLogger } from "./middlewares/myLogger";
 import rootReducer from "./rootReducer";
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(myLogger, logger))
+);
 
 export default store;
