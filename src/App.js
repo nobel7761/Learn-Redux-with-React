9563556a2e5fb703f865887d325a01b2ff58f9a4
nobel7761 <habiburnobel@gmail.com>
2023-02-27 02:09:24 +0600

    folder structure completed for the project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import { Provider } from "react-redux";
-// import Counter from "./components/Counter";
-import DynamicHooksCounter from "./components/DynamicHooksCounter";
-import HooksCounter from "./components/HooksCounter";
-import VariableDynamicHooksCounter from "./components/VariableDynamicHooksCounter";
-import store from "./redux/store";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import TodoList from "./components/TodoList";
 
-export default function App() {
+function App() {
   return (
-    <Provider store={store}>
-      <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
-        <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
-          Simple Counter Application
-        </h1>
+    <div className="grid place-items-center bg-blue-100 h-screen px-6 font-sans">
+      <Navbar />
 
-        <div className="max-w-md mx-auto mt-10 space-y-5">
-          {/* <Counter id="1" /> */}
-          <HooksCounter />
-          <DynamicHooksCounter />
-          <VariableDynamicHooksCounter />
-          <VariableDynamicHooksCounter dynamic />
-        </div>
+      <div className="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
+        <Header />
+
+        <hr className="mt-4" />
+
+        <TodoList />
+
+        <hr className="mt-4" />
+
+        <Footer />
       </div>
-    </Provider>
+    </div>
   );
 }
+
+export default App;
